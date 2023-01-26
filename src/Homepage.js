ad5854cec278b049807df32e42cf1dd169899d9e
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-01-26 13:12:45 +0530

    fixed homepage styling

--- src/Homepage.js
+++ src/Homepage.js
@@ -13,61 +13,70 @@ function Homepage() {
       <Nav />
 
       <article className="hero_section">
-        <section>
-          <p className="branding">Infrastructure for the cnnected world.</p>
-          <p className="subheading">
-            Register with us today and experience the difference in Internet
-            service.
-          </p>
-          <Button text="Register Now" />
+        <section className="div-center">
+          <div>
+            <div className="hero_text">
+              <p className="branding">
+                Infrastructure for the connected world.
+              </p>
+              <p className="subheading">
+                Register with us today and experience the difference in Internet
+                service.
+              </p>
+            </div>
+            <Button text="Register Now" />
+          </div>
         </section>
         <img src={HeroImg} alt="Two people talking to each other" />
       </article>
 
-      <section className="section">
-        <div>
-          We provide affordable high-speed internet access to college students.
-          We understand the importance of staying connected and having access to
-          reliable internet, which is why we strive to offer the best service at
-          the most affordable price.
-        </div>
-        <img
-          src={OfficeImg}
-          alt="A laptop placed on a table using high speed internet"
-        />
-      </section>
-
-      <section className="qna">
-        <h2>
-          Why <span>Prokits</span>?
-        </h2>
-        <div>
-          <p>
-            Our plans are tailored to meet the needs of college students, with
-            fast and reliable internet speeds to help you stay connected and
-            productive. Thank you for considering our company for your internet
-            service needs. Sign up today and experience the difference.
-          </p>
-          <p>
-            We have a dedicated team of experts who are always ready to assist
-            you with any questions or concerns you may have. We are committed to
-            providing a seamless and stress-free experience for our customers.
-          </p>
-        </div>
-        <div>
+      <div className="container">
+        <section className="section">
+          <div>
+            We provide affordable high-speed internet access to college
+            students. We understand the importance of staying connected and
+            having access to reliable internet, which is why we strive to offer
+            the best service at the most affordable price.
+          </div>
           <img
-            src={MessageImg}
-            alt="A person sending email using mobile phone"
+            src={OfficeImg}
+            alt="A laptop placed on a table using high speed internet"
           />
-          <p className="bold">
-            We also understand the importance of security and privacy when it
-            comes to internet service. That's why we take measures to ensure
-            your personal information and online activity is kept secure and
-            private.
-          </p>
-        </div>
-      </section>
+        </section>
 
+        <section className="qna">
+          <h2>
+            Why <span>Prokits</span>?
+          </h2>
+          <div>
+            <p>
+              Our plans are tailored to meet the needs of college students, with
+              fast and reliable internet speeds to help you stay connected and
+              productive. Thank you for considering our company for your
+              internet service needs. Sign up today and experience the
+              difference.
+            </p>
+            <p>
+              We have a dedicated team of experts who are always ready to assist
+              you with any questions or concerns you may have. We are committed
+              to providing a seamless and stress-free experience for our
+              customers.
+            </p>
+          </div>
+          <div>
+            <img
+              src={MessageImg}
+              alt="A person sending email using mobile phone"
+            />
+            <p className="bold">
+              We also understand the importance of security and privacy when it
+              comes to internet service. That's why we take measures to ensure
+              your personal information and online activity is kept secure and
+              private.
+            </p>
+          </div>
+        </section>
+      </div>
       <Footer />
     </>
   );
